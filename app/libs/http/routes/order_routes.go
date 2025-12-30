578e5b3c0f22cfb92cefe6a3c5e7a46fc5aae86d
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2025-12-30 00:52:39 -0300

    refactor: remove unused Patch method and related logic from order_controller and order_case

--- app/libs/http/routes/order_routes.go
+++ app/libs/http/routes/order_routes.go
@@ -9,7 +9,6 @@ import (
 func Order(app *fiber.App, c *order_controller.OrderController) {
 	route := app.Group("/order")
 	route.Post("/", c.Create)
-	route.Patch("/:id", c.Patch)
 	route.Delete("/:id", c.Delete)
 	route.Get("/:id", c.GetByID)
 	route.Get("/", c.Paginate)
