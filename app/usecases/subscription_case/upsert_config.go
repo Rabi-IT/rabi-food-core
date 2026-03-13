71e32beefac3798c8571340ceff98d2d35c17276
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2026-03-13 19:02:12 -0300

    refactor: update UpsertConfig method signatures to return error only

--- app/usecases/subscription_case/upsert_config.go
+++ app/usecases/subscription_case/upsert_config.go
@@ -6,14 +6,8 @@ import (
 	g "rabi-food-core/libs/database/gateways/subscription_gateway"
 )
 
-func (s *SubscriptionCase) UpsertConfig(ctx context.Context, input g.UpsertConfigInput) (bool, error) {
+func (s *SubscriptionCase) UpsertConfig(ctx context.Context, input g.UpsertConfigInput) error {
 	session := app_context.GetSession(ctx)
 
-	upserted, err := s.gateway.UpsertConfig(ctx, session.TenantID, input)
-
-	if err != nil {
-		return false, err
-	}
-
-	return upserted, nil
+	return s.gateway.UpsertConfig(ctx, session.TenantID, input)
 }
