30b4051e21600eff631d3c982d49e302fe3874ab
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2026-03-14 14:05:05 -0300

    chore: add favicon link to Scalar API documentation

--- app/libs/docs/scalar_ui.go
+++ app/libs/docs/scalar_ui.go
@@ -14,6 +14,7 @@ func ServeUI(c *fiber.Ctx) error {
 		<meta
 		name="viewport"
 		content="width=device-width, initial-scale=1" />
+		<link rel="icon" type="image/png" href="/docs/favicon.png" />
 	</head>
 	<body>
 		<div id="app"></div>
