770d3e9bbebfbbae7dc69a37f6d6023643a0fb79
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2026-03-14 13:00:48 -0300

    chore: add ServeUI function to serve API documentation and integrate it into the fiber adapter

---
+++ app/libs/docs/ui.go
@@ -0,0 +1,32 @@
+package docs
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func ServeUI(c *fiber.Ctx) error {
+	html := `
+	<!doctype html>
+	<html>
+	<head>
+		<title>Scalar API Reference</title>
+		<meta charset="utf-8" />
+		<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1" />
+	</head>
+	<body>
+		<div id="app"></div>
+		<script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+		<script>
+		Scalar.createApiReference('#app', {
+			url: '/docs/swagger.json',
+		})
+		</script>
+	</body>
+	</html>`
+
+	c.Set("Content-Type", "text/html")
+
+	return c.SendString(html)
+}
